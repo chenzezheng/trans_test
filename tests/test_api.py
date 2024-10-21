d1bb4699561caa11a10b35db407ec4b3b3d54c47
Akakii <akakiichan@foxmail.com>
2024-10-21 14:13:56 +0800

    add image translation

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,11 +14,11 @@ def test_text_translation(client):
     response = client.post('/text', json=data)
     assert response.status_code == 200
 
-# def test_image_translation(client):
-#     form = {
-#         "from": "en",
-#         "to": "zh",
-#         "image": (Path(__file__).parent / "test.png").open("rb")
-#     }
-#     response = client.post('/image', data=form)
-#     assert response.status_code == 200
+def test_image_translation(client):
+    form = {
+        "from": "en",
+        "to": "zh",
+        "image": (Path(__file__).parent / "test.png").open("rb")
+    }
+    response = client.post('/image', data=form)
+    assert response.status_code == 200
